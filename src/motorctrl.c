2c36965a23bf6be72b2f1246193efd8315b7a16a
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-09-30 12:57:55 +0300

    Remove unused spinlock implementation

--- src/motorctrl.c
+++ src/motorctrl.c
@@ -8,7 +8,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#include "spinlock.h"
+
 #include "pid.h"
 
 #define ARRAY_LEN(a) (sizeof(a) / sizeof(a[0]))
--- src/spinlock.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPINLOCK_H
-#define SPINLOCK_H
-typedef volatile int spinlock;
-void spin_lock(spinlock *lock);
-void spin_unlock(spinlock *lock);
-#endif
