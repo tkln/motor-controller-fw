be4513cde5feca02a74444d205f8e59e758e4cfe
Aapo Vienamo <avienamo@iki.fi>
2018-10-20 23:55:25 +0300

    Set baudrate to 576000

--- src/motorctrl.c
+++ src/motorctrl.c
@@ -168,7 +168,7 @@ static void uart_setup(void)
     gpio_mode_setup(GPIOD, GPIO_MODE_AF, GPIO_PUPD_NONE, GPIO8 | GPIO9);
     gpio_set_af(GPIOD, GPIO_AF7, GPIO8 | GPIO9);
     rcc_periph_clock_enable(RCC_USART3);
-    usart_set_baudrate(USART3, 115200);
+    usart_set_baudrate(USART3, 576000);
     usart_set_databits(USART3, 8);
     usart_set_stopbits(USART3, USART_STOPBITS_1);
     usart_set_mode(USART3, USART_MODE_TX_RX);
